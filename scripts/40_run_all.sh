4570045644b542372f8edc976f89e6b006c7b6f3
Ryan7777 <Ryan7777@node1.ryan.cs740-uwmadison-pg0.utah.cloudlab.us>
2025-11-28 15:13:18 -0700

    full date

--- scripts/40_run_all.sh
+++ scripts/40_run_all.sh
@@ -7,7 +7,8 @@ if [[ -z "${RESOLVER_IP:-}" ]]; then
 fi
 
 modes=$(yq -r '.modes[]' config/modes.yml)
-mapfile -t sites < <(shuf config/sites.txt)
+# mapfile -t sites < <(shuf config/sites.txt)
+mapfile -t sites < <(shuf config/unpopular_sites.txt)
 
 for mode in $modes; do
   ./scripts/10_dns_profiles.sh "$mode"
@@ -16,13 +17,13 @@ for mode in $modes; do
     # COLD
     ssh "Ryan7777@$RESOLVER_IP" 'sudo unbound-control flush_zone . || sudo systemctl restart unbound' || true
     sudo resolvectl flush-caches || true
-    ./scripts/20_measure_dns.sh "$site" "$RESOLVER_IP" "$mode" data/raw/local_cache_dns_cold.csv 5
+    ./scripts/20_measure_dns.sh "$site" "$RESOLVER_IP" "$mode" new_data/unpop_raw/doh_dns_cold.csv 5
     tmpdir=$(mktemp -d)
-    node scripts/30_measure_pageload.js "$site" "$mode" data/raw/local_cache_web_cold.csv "$tmpdir"
+    node scripts/30_measure_pageload.js "$site" "$mode" new_data/unpop_raw/doh_web_cold.csv "$tmpdir"
     rm -rf "$tmpdir"
     # WARM
     node scripts/30_measure_pageload.js "$site" "$mode" /dev/null "$HOME/.warm-$mode-$site" || true
-    ./scripts/20_measure_dns.sh "$site" "$RESOLVER_IP" "$mode" data/raw/local_cache_dns_warm.csv 5
-    node scripts/30_measure_pageload.js "$site" "$mode" data/raw/local_cache_web_warm.csv "$HOME/.warm-$mode-$site" || true
+    ./scripts/20_measure_dns.sh "$site" "$RESOLVER_IP" "$mode" new_data/unpop_raw/doh_dns_warm.csv 5
+    node scripts/30_measure_pageload.js "$site" "$mode" new_data/unpop_raw/doh_web_warm.csv "$HOME/.warm-$mode-$site" || true
   done
 done
